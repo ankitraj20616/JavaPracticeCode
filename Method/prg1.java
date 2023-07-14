49711c7f356dfc923a3604e8c172fd94cfaa9f8d
Ankit <ankitrajthakur803@gmail.com>
2023-07-14 13:38:35 +0530

    java question

---
+++ Method/prg1.java
@@ -0,0 +1,10 @@
+package Method;
+
+public class prg1 {
+    static void display(){
+        System.out.println("This the use of void keyword in java!");
+    }
+    public static void main(String[] args) {
+        display();
+    }
+}
